7f647cc85c1caa61109658f15aaaefa22619ec77
Mateusz Zalega <mateusz@radicallyopensecurity.com>
2016-05-25 14:55:30 +0200

    connectivity: fixed Internet connection test
    Signed-off-by: Mateusz Zalega <mateusz@radicallyopensecurity.com>

--- root/usr/share/nakd/scripts/connectivity/internet/00google.sh
+++ root/usr/share/nakd/scripts/connectivity/internet/00google.sh
@@ -1,9 +1,2 @@
 #!/bin/sh
-
-nbytes=$(wget -qO- --no-check-certificate --no-dns-cache http://clients3.google.com/generate_204 | wc -c)
-
-if [ $nbytes -ne 0 ]; then
-    echo "yes"
-else
-    echo "no"
-fi
+wget --timeout=5 -qO- --no-check-certificate --no-dns-cache http://clients3.google.com/generate_204
