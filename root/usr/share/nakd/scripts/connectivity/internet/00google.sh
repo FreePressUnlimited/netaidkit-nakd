e57c54b498648b27e3bad3d3434d85860f35134c
Mateusz Zalega <mateusz@radicallyopensecurity.com>
2016-06-04 17:01:01 +0200

    connectivity: added arping timeout
    Signed-off-by: Mateusz Zalega <mateusz@radicallyopensecurity.com>

--- root/usr/share/nakd/scripts/connectivity/internet/00google.sh
+++ root/usr/share/nakd/scripts/connectivity/internet/00google.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-wget --timeout=5 -qO- --no-check-certificate --no-dns-cache http://clients3.google.com/generate_204
+timeout -t 5 wget --timeout=5 -qO- --no-check-certificate --no-dns-cache http://clients3.google.com/generate_204
