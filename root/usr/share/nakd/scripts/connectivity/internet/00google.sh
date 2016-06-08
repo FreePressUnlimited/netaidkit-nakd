97b3d0e000ad92c39845e0191196f0667cf784af
Mateusz Zalega <mateusz@radicallyopensecurity.com>
2016-06-08 14:04:26 +0200

    connectivity: fixed Internet connectivity test
    Signed-off-by: Mateusz Zalega <mateusz@radicallyopensecurity.com>

--- root/usr/share/nakd/scripts/connectivity/internet/00google.sh
+++ root/usr/share/nakd/scripts/connectivity/internet/00google.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-timeout -t 5 wget --timeout=5 -qO- --no-check-certificate --no-dns-cache http://clients3.google.com/generate_204
+wget --timeout=5 -qO- --no-check-certificate --no-dns-cache http://clients3.google.com/generate_204
