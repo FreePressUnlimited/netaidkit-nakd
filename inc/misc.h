f5519dafdb1ef15ee865e646261a06d976392d77
Mateusz Zalega <mateusz@radicallyopensecurity.com>
2016-06-10 20:19:01 +0200

    misc: added monotonic_time()
    Signed-off-by: Mateusz Zalega <mateusz@radicallyopensecurity.com>

--- inc/misc.h
+++ inc/misc.h
@@ -1,8 +1,11 @@
 #ifndef NAKD_MISC_H
 #define NAKD_MISC_H
+#include <time.h>
 
 #define N_ELEMENTS(arr) (sizeof(arr) / sizeof(arr[0]))
 #define ARRAY_END(arr) (arr + N_ELEMENTS(arr))
 #define ARRAY_ELEMENT_NUMBER(ptr, arr) ((int)(ptr - arr))
+
+time_t monotonic_time(void);
 
 #endif
--- misc.c
+++ misc.c
@@ -1,1 +1,9 @@
+#include <time.h>
 #include "misc.h"
+#include "log.h"
+
+time_t monotonic_time(void) {
+    struct timespec ts;
+    nakd_assert(clock_gettime(CLOCK_MONOTONIC, &ts));
+    return ts.tv_sec;
+}
