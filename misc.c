eaaaae2d0d58f9f5fb91347d07bbcaea04523941
Mateusz Zalega <mateusz@radicallyopensecurity.com>
2016-06-13 17:02:31 +0200

    Fixed incorrect clock_gettime assertion
    Signed-off-by: Mateusz Zalega <mateusz@radicallyopensecurity.com>

--- misc.c
+++ misc.c
@@ -4,6 +4,6 @@
 
 time_t monotonic_time(void) {
     struct timespec ts;
-    nakd_assert(clock_gettime(CLOCK_MONOTONIC, &ts));
+    nakd_assert(!clock_gettime(CLOCK_MONOTONIC, &ts));
     return ts.tv_sec;
 }
