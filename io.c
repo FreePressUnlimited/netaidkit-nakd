2df9f15b0be5d8eead9b4525eb848b6940b564fa
Mateusz Zalega <mateusz@radicallyopensecurity.com>
2016-08-15 17:49:15 +0200

    io: correct thread argument handling
    Signed-off-by: Mateusz Zalega <mateusz@radicallyopensecurity.com>

--- io.c
+++ io.c
@@ -59,7 +59,7 @@ static void _poll_shutdown(struct nakd_thread *thread) {
 
 static int _io_init(void) {
     nakd_assert(!nakd_thread_create_joinable(_poll_loop,
-         _poll_shutdown, NULL, (void *)(_poll_thread)));
+                  _poll_shutdown, NULL, &_poll_thread));
     nakd_assert((_efd = epoll_create1(EPOLL_CLOEXEC)) != -1);
     return 0;
 }
