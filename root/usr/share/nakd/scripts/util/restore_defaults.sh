d985b9ac30233d28cc86488df9274cc74ac13ff5
Mateusz Zalega <mateusz@radicallyopensecurity.com>
2016-08-31 15:26:52 +0200

    stage: reset: fixed a typo
    Signed-off-by: Mateusz Zalega <mateusz@radicallyopensecurity.com>

--- root/usr/share/nakd/scripts/util/restore_defaults.sh
+++ root/usr/share/nakd/scripts/util/restore_defaults.sh
@@ -2,7 +2,7 @@
 # daemon config
 cp -raf /usr/share/nakd/defaults/* /
 # device, firewall, network config
-cp -raf /usr/share/nak/defaults/etc/config/* /etc/config/*
+cp -raf /usr/share/nak/defaults/etc/config/* /etc/config/
 # uploaded openvpn certificates
 rm -rf /nak/ovpn/upload/*
 # webapp configuration
