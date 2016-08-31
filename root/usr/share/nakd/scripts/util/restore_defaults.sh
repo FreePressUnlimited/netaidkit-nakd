e3e921733f57177616038a523b273f84290da124
Mateusz Zalega <mateusz@radicallyopensecurity.com>
2016-08-31 13:42:24 +0200

    Restore /etc during factory reset
    Signed-off-by: Mateusz Zalega <mateusz@radicallyopensecurity.com>

--- root/usr/share/nakd/scripts/util/restore_defaults.sh
+++ root/usr/share/nakd/scripts/util/restore_defaults.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+# daemon config
 cp -raf /usr/share/nakd/defaults/* /
+# project config
+cp -raf /usr/share/nak/defaults/* /
+# uploaded openvpn certificates
 rm -rf /nak/ovpn/upload/*
+# webapp configuration
 rm -rf /nak/webapp/data/*
