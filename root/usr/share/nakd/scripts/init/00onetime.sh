d4133c77c876b540336271f1a93f5fba689715d1
Mateusz Zalega <mateusz@radicallyopensecurity.com>
2017-01-23 12:39:42 +0100

    Adjusted the onetime firmware update script
    Signed-off-by: Mateusz Zalega <mateusz@radicallyopensecurity.com>

--- root/usr/share/nakd/scripts/init/00onetime.sh
+++ root/usr/share/nakd/scripts/init/00onetime.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-cp /usr/share/nak/defaults/etc/config/firewall /etc/config/.
-cp /usr/share/nak/defaults/etc/config/network /etc/config/.
-cp /usr/share/nak/defaults/etc/config/wireless /etc/config/.
+/usr/share/nakd/scripts/util/restore_defaults.sh
+
+# Make sure /run/tor exists and is accessible only by "tor" user,
+# otherwise Tor daemon will terminate.                           
+mkdir -p /run/tor
+chown tor:tor /run/tor
+chmod 700 /run/tor
+
 rm /usr/share/nakd/scripts/init/00onetime.sh
