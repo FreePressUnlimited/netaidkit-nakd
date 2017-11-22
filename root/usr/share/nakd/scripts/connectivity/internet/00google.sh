2eb785e5bc122fba6137e178c03499bf0cd646df
Mateusz Zalega <mateusz@appliedsourcery.com>
2017-11-22 13:31:22 +0100

    Check HTTP response code in Internet connectivity test
    Signed-off-by: Mateusz Zalega <mateusz@appliedsourcery.com>

--- root/usr/share/nakd/scripts/connectivity/internet/00google.sh
+++ root/usr/share/nakd/scripts/connectivity/internet/00google.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-wget --timeout=5 -qO- --no-check-certificate --no-dns-cache http://clients3.google.com/generate_204
+curl -o /dev/null --silent --head --write-out '%{http_code}\n' http://clients3.google.com/generate_204 | awk '{if ($0 == "204") exit 0; exit 1;}'
